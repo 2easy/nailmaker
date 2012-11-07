23d3079972c33906ff5c33a1e7f1e2abbbd136d9
Piotr Olchawa <piotrekolchawa@gmail.com>
2012-11-07 09:51:14 +0100

    add welcome page and locales

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nailmaker::Application.config.secret_token = '609840061a923fbef83c360cc469ddfb372544eacf00ac1acf4583f03dc05ca6d5b5cd4588a5aaa9136003ddee85fa7c8d5bb555e157ff4e242bb6f633a0163b'
